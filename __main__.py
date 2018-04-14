83a5288733757e452d4b57895c1a06d827277066
MattQuintana <mq56@nau.edu>
2018-04-14 14:56:37 -0700

    Updating main code and removing .idea folder

--- __main__.py
+++ __main__.py
@@ -14,40 +14,42 @@ redditConfig = Config()
 reddit = redditConfig.getReddit()
 
 def main():
-    subreddits = {}
+    #generate_data()
+    pass
 
-    for submission in reddit.front.top(limit=1000):
-        '''
-        print(submission.title)  # Output: the submission's title
-        print(submission.score)  # Output: the submission's score
-        print(submission.id)     # Output: the submission's ID
-        print(submission.url)    # Output: the URL the submission points to
-                                 # or the submission's URL if it's a self post
-        print(submission.author)
-        print(submission.subreddit)
-        print(get_time(submission))'''
+# Generate a series of data and put them into files
+def generate_data():
+    types = ["top", "hot"]
+    amount = [10, 100, 1000]
 
-        subreddit_name = submission.subreddit.display_name
-        if subreddit_name not in subreddits:
-            subreddits[subreddit_name] = 1
-        else:
-            subreddits[subreddit_name] += 1
+    # For each type of submission
+    for type in types:
+        # For each amount we need to grab
+        for a in amount:
+            # Create a list to hold the full dictionary of data about a submission
+            submission_list = []
+            # The fields we want to know about
+            fields = ('title', 'score', 'id', 'author', 'subreddit', 'created', 'url')
 
-    print(subreddits)
-    sorted_subs = sorted(subreddits.items(), key = operator.itemgetter(1))
-    sorted_subs.reverse()
-    print(sorted_subs)
+            # For every submission that we want to see
+            for submission in reddit.front.top(limit=a):
+                # Get the different variables that pertain to it
+                to_dict = vars(submission)
+                # Create a dictionary that contains all of the fields that we want
+                # and their related values in the submission
+                sub_dict = {field:str(to_dict[field]) for field in fields}
+                # Add it to the list of submissions
+                submission_list.append(sub_dict)
 
+            # Create the filename that we want to save each set into
+            filename = type+str(a)+"_data.json"
+            with open(filename, 'w+', encoding='utf-16') as f:
+                # For every submission
+                for submission in submission_list:
+                    f.write(str(submission))
+                    f.write("\n")
 
-
-def get_time(submission):
-    time = submission.created
-    return datetime.datetime.fromtimestamp(time)
-
-
-
-
-
-main()
+if __name__ == "__main__":
+    main()
 
 
